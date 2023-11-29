ab8b7cb899f18334c05d1cd2057afaf61de28328
Alex Jarabi <jarabialex@gmail.com>
2023-11-29 17:24:43 +0300

    Correction of error checks

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,15 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((response) => {
-      const [uploadResponse, newUserResponse] = response;
-      console.log(
-        uploadResponse.body,
-        newUserResponse.firstName,
-        newUserResponse.lastName,
-      );
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch((error) => {
+    .catch(() => {
       console.log('Signup system offline');
     });
 }
