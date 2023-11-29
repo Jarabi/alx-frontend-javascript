42c90a8876b58c7f3c86b3889651d036497fcf8b
Alex Jarabi <jarabialex@gmail.com>
2023-11-30 01:38:31 +0300

    Added status checks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,19 +2,21 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const responses = [];
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((results) => {
-    results.map((result) => {
-      responses.push({
-        status: result.status,
-        value:
-          result.status === 'rejected'
-            ? result.reason.toString()
-            : result.value,
-      });
+    const responses = [];
+
+    results.forEach((result) => {
+      if (result.status === 'fulfilled') responses.push(result);
+
+      if (result.status === 'rejected') {
+        responses.push({
+          status: result.status,
+          value: result.reason,
+        });
+      }
     });
     return responses;
   });
