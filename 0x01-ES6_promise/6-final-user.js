aea8bd8f8846674e0699f92c6a675a7741a2df4e
Alex Jarabi <jarabialex@gmail.com>
2023-11-30 01:51:07 +0300

    Final correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,19 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => {
-    const responses = [];
-
-    results.forEach((result) => {
-      if (result.status === 'fulfilled') responses.push(result);
-
-      if (result.status === 'rejected') {
-        responses.push({
-          status: result.status,
-          value: result.reason,
-        });
-      }
-    });
-    return responses;
-  });
+  ]).then((results) => results);
 }
