118bc62745f8be658b94e0313a0af9059780f82e
Alex Jarabi <jarabialex@gmail.com>
2023-11-30 01:05:59 +0300

    Correction on returned array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,20 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const responses = [];
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((results) => {
-    results.map((result) => ({
-      status: result.status,
-      value:
-        result.status === 'rejected' ? result.reason.toString() : result.value,
-    }));
+    results.map((result) => {
+      responses.push({
+        status: result.status,
+        value:
+          result.status === 'rejected'
+            ? result.reason.toString()
+            : result.value,
+      });
+    });
+    return responses;
   });
 }
