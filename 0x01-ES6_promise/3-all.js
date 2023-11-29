29e4710ce45f0d2070e39b9ba00f2991442efc32
Alex Jarabi <jarabialex@gmail.com>
2023-11-29 17:35:35 +0300

    Forgot the return statement. Now added

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-unused-vars */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
