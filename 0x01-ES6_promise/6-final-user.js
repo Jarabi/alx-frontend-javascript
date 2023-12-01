70dde8241dbd4e41dca6c7577157beae3b4bda61
Alex Jarabi <jarabialex@gmail.com>
2023-12-01 13:52:26 +0300

    Linting correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(
   firstName,
   lastName,
-  fileName
+  fileName,
 ) {
   const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
