c32cb65a00ad5aa0f6aee7f9f74f737f185c06b0
Alex Jarabi <jarabialex@gmail.com>
2023-12-08 11:59:53 +0300

    Adjusted max queries

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,5 +1,5 @@
 const weakMap = new WeakMap();
-const MAX_QUERIES = 5;
+const MAX_QUERIES = 4;
 
 const queryAPI = (endPoint) => {
   if (!weakMap.has(endPoint)) {
@@ -10,7 +10,9 @@ const queryAPI = (endPoint) => {
 
   weakMap.set(endPoint, queries + 1);
 
-  if (queries >= MAX_QUERIES) throw new Error('Endpoint load is high');
+  if (queries >= MAX_QUERIES) {
+    throw new Error('Endpoint load is high');
+  }
 };
 
 export { queryAPI, weakMap };
